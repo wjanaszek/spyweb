a5d59ff4b6caf3d31816df96f96986d3225f044c
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2018-05-23 23:50:52 +0200

    Removed default test values in db users and tasks table

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO ADMINS(LOGIN, PASSWORD, LOGGED_IN) VALUES ('admin', 'admin', false);
 INSERT INTO ADMINS(LOGIN, PASSWORD) VALUES ('sa', 'sa');
-
-INSERT INTO USERS(NAME, PASSWORD) VALUES ('user', 'user');
-
-INSERT INTO TASKS(USER_ID, NAME, FILE_URL) VALUES (1, 'photo', '/Users/wjanaszek/Downloads/1983_04_29_Razem_Prasa_SP303-3.jpg');
+--
+-- INSERT INTO USERS(NAME, PASSWORD) VALUES ('user', 'user');
+--
+-- INSERT INTO TASKS(USER_ID, NAME, FILE_URL) VALUES (1, 'photo', '/Users/wjanaszek/Downloads/1983_04_29_Razem_Prasa_SP303-3.jpg');
 
